47f2b7fcd62f9af7077d6d5248e7ceb312ff73ca
tooppoo <tooppoo56@gmail.com>
2016-05-01 03:31:36 +0900

    csv出力時の実行日時を、CreatedAtから取得する形に変更

--- vmstat_cat/core/io/vmstat_writer.rb
+++ vmstat_cat/core/io/vmstat_writer.rb
@@ -58,23 +58,6 @@ module VmstatCat
           FileUtils::mkdir_p(@dir)
         end
       end
-      
-      def set_created_at
-        fill_0 = lambda{|val|
-          format("%02d", val)
-        }
-        oclock = Time.now
-        
-        y = oclock.year.to_s
-        m = fill_0.call(oclock.month)
-        d = fill_0.call(oclock.day)
-        
-        h = fill_0.call(oclock.hour)
-        mi = fill_0.call(oclock.min)
-        s = fill_0.call(oclock.sec)
-        
-        "#{y + m + d}_#{h + mi + s}"
-      end
     end
   end
 end
