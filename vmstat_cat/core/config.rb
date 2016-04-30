4c9ceed170ccd818be3bb446c6a73aea51550298
tooppoo <tooppoo56@gmail.com>
2016-05-01 00:05:23 +0900

    loggerを追加

--- vmstat_cat/core/config.rb
+++ vmstat_cat/core/config.rb
@@ -1,5 +1,7 @@
 #encoding: utf-8
 
+require 'logger'
+
 module VmstatCat
   module Config
     test_mode = $0 == __FILE__
@@ -9,7 +11,26 @@ module VmstatCat
       puts "--------------------------------"
     }
     check_require_path.call('before') if test_mode
-    $: << File.expand_path(__dir__)
+    $: << File::expand_path(__dir__)
     check_require_path.call('after') if test_mode
+    
+    module Logger
+      @logger = nil
+      def get
+        @logger ||= Logger.new(File::expand_path("../log/log_${get_today}", __dir__))
+        @logger.level = Logger::DEBUG
+        @logger
+      end
+      
+      def get_today
+        today = Date.today
+        
+        y = today.year.to_s
+        m = format("%02d", today.month)
+        d = format("%02d", today.day)
+        
+        y + m + d
+      end
+    end
   end
 end
