c89c9f19f3341491c1f940e1c1a0778eba9d1524
tooppoo <tooppoo56@gmail.com>
2016-05-01 13:09:19 +0900

    区切り文字を取得するメソッドを追加

--- vmstat_cat/core/app_logger.rb
+++ vmstat_cat/core/app_logger.rb
@@ -10,7 +10,12 @@ module VmstatCat
     def self.get
       @@logger = Logger.new(File::expand_path("../log/log_#{created_at}.log", __dir__)) unless defined? @@logger
       @@logger.level = Logger::DEBUG
+      
       @@logger
+    end
+    
+    def self.delimiter(deli = '-', count = 80)
+      deli * count
     end
   end
 end
