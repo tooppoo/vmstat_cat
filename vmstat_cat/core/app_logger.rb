893d63bf5b5ec45c556f7838aaa0658305818d40
tooppoo <Ryouta@kuribayashiryouta-no-MacBook-Pro.local>
2016-05-03 18:19:44 +0900

    logger取得時、vmstat_cat/logが存在しなければ作成する処理を追加

--- vmstat_cat/core/app_logger.rb
+++ vmstat_cat/core/app_logger.rb
@@ -8,6 +8,8 @@ module VmstatCat
     extend CreatedAt
     
     def self.get
+      self::ensure_directory
+      
       @@logger = Logger.new(File::expand_path("../log/log_#{created_at}.log", __dir__)) unless defined? @@logger
       @@logger.level = Logger::DEBUG
       
@@ -16,6 +18,12 @@ module VmstatCat
     
     def self.delimiter(deli = '-', count = 80)
       deli * count
+    end
+
+    private
+    def self.ensure_directory
+      log_dir = File::expand_path('../log', __dir__)
+      Dir::mkdir(log_dir) unless Dir::exists?(log_dir)
     end
   end
 end
