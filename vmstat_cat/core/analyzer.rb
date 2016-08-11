b5adee80a9d3ac1ab9f99f4adb179799b954ef7b
tooppoo <Ryouta@kuribayashiryouta-no-MacBook-Pro.local>
2016-08-11 16:43:11 +0900

    テストデータを修正

--- vmstat_cat/core/analyzer.rb
+++ vmstat_cat/core/analyzer.rb
@@ -9,33 +9,33 @@ module VmstatCat
   # 静的メンバしか持たないため、単一オブジェクトとする。
   class Analyzer
     include Singleton
-    
+
     @@logger = AppLogger::get
-    
+
     module Part
       @@logger = AppLogger::get
-      
+
       Header = lambda{|read_data|
         @@logger.info('Analyzer::execute : Header')
-        
+
         result = { :page_size => extraction(read_data.header) }
-        
+
         @@logger.debug(result)
         result
       }.freeze
       Body = lambda{|read_data|
         @@logger.info('Analyzer::execute : Body')
         body_data = read_data.body
-        
+
         result = {}
-        
+
         read_data.body.each{|data|
-          key = data.match(/.+:/)[0].gsub(":", "")
+          key = data.match(/.+:/)[0].gsub(/[:"]/, "")
           value = data.match(/[0-9]+/)[0]
-          
+
           @@logger.debug("key:#{key}")
           @@logger.debug("value:#{value}")
-          
+
           result[key.to_s.to_sym] = value.to_s
         }
 
@@ -44,17 +44,17 @@ module VmstatCat
       }.freeze
       Footer = lambda{|read_data|
         @@logger.info('Analyzer::execute : Footer')
-        
+
         unless Config::FOOTER_EXISTS then
           @@logger.info('Analyzer::execute : Footer doesn`t exist')
           return {}
         end
-  
+
         footer_ext = lambda{|mark|
           matched = read_data.footer.match(/[0-9]+ ?#{mark}/)
           matched.nil? ? "" : matched[0].gsub(/[^0-9]+/, "").to_i
         }
-        
+
         result = {
           :cache    => footer_ext.call('lookups'),
           :hits     => footer_ext.call('hits'),
@@ -65,32 +65,32 @@ module VmstatCat
       }.freeze
       All = lambda{|read_data|
         @@logger.info('Analyzer::execute : All')
-        
+
         header = Header.call(read_data)
         body = Body.call(read_data)
         footer = Footer.call(read_data)
-        
+
         result = {}.merge(header).merge(body).merge(footer)
         @@logger.debug(result)
         result
       }.freeze
-      
+
       private
       def self.extraction(str)
         str.gsub(/[^0-9]/, "").to_i
       end
     end
     Part.freeze
-    
+
     def self.execute(read_data, part = Part::All)
       @@logger.info(AppLogger::delimiter)
       @@logger.info('Analyzer::execute start')
-      
+
       if read_data.nil? || read_data.empty? then
         @@logger.info('Analyzer::execute data is empty. finish')
         return {}
       end
-      
+
       result = part.call(read_data)
       @@logger.info('Analyzer::execute finish')
       result
--- vmstat_cat/test/analyzer.rb
+++ vmstat_cat/test/analyzer.rb
@@ -15,33 +15,45 @@ class TestAnalyzer < Test::Unit::TestCase
     reader = VmstatReader.new(sample_log_path)
     @read_data = reader.read_single
   end
-  
+
   def test_analyze_header
     expected = {:page_size => 4096}
     actual = Analyzer::execute(@read_data, Analyzer::Part::Header)
-    
+
     assert_equal(expected, actual)
   end
-  
+
   def test_analyze_body
     expected = {
-      :free => 19754,
-      :active => 701005,
-      :inactive => 1103397,
-      :speculative => 6770,
-      :wired_down => 265668,
-      :translation_faults => 108470704,
-      :copy_on_write => 12199065,
-      :zero_filled => 54393992,
-      :reactivated => 55794,
-      :pageins => 3953684,
-      :pageouts => 2303
+
+      :"Pages free"                     =>        "538494",
+      :"Pages active"                   =>        "857164",
+      :"Pages inactive"                 =>         "96087",
+      :"Pages speculative"             =>         "73793",
+      :"Pages throttled"                =>             "0",
+      :"Pages wired down"               =>        "252371",
+      :"Pages purgeable"                =>        "207561",
+      :"Translation faults"             =>      "41234592",
+      :"Pages copy-on-write"            =>        "944859",
+      :"Pages zero filled"              =>      "25838868",
+      :"Pages reactivated"              =>        "420552",
+      :"Pages purged"                   =>        "282765",
+      :"File-backed pages"              =>        "208787",
+      :"Anonymous pages"                =>        "818257",
+      :"Pages stored in compressor"     =>        "587616",
+      :"Pages occupied by compressor"   =>        "278734",
+      :Decompressions                 =>       "1107375",
+      :Compressions                   =>       "1904226",
+      :Pageins                        =>       "2532187",
+      :Pageouts                       =>          "1587",
+      :Swapins                        =>       "1062813",
+      :Swapouts                       =>       "1093920"
     }
     actual = Analyzer::execute(@read_data, Analyzer::Part::Body)
-    
+
     assert_equal(expected, actual)
   end
-  
+
   def test_analyze_footer
     expected = {
       :cache => 579187,
@@ -50,35 +62,43 @@ class TestAnalyzer < Test::Unit::TestCase
     }
     actual = Analyzer::execute(@read_data, Analyzer::Part::Footer)
   end
-  
+
   def test_analyze
     expected = {
-      :page_size => 4096,
-      :free => 19754,
-      :active => 701005,
-      :inactive => 1103397,
-      :speculative => 6770,
-      :wired_down => 265668,
-      :translation_faults => 108470704,
-      :copy_on_write => 12199065,
-      :zero_filled => 54393992,
-      :reactivated => 55794,
-      :pageins => 3953684,
-      :pageouts => 2303,
-      :cache => 579187,
-      :hits => 15,
-      :hit_rate => 0
+      :page_size    => 4096,
+      :"Pages free"                     =>        "538494",
+      :"Pages active"                   =>        "857164",
+      :"Pages inactive"                 =>         "96087",
+      :"Pages speculative"             =>         "73793",
+      :"Pages throttled"                =>             "0",
+      :"Pages wired down"               =>        "252371",
+      :"Pages purgeable"                =>        "207561",
+      :"Translation faults"             =>      "41234592",
+      :"Pages copy-on-write"            =>        "944859",
+      :"Pages zero filled"              =>      "25838868",
+      :"Pages reactivated"              =>        "420552",
+      :"Pages purged"                   =>        "282765",
+      :"File-backed pages"              =>        "208787",
+      :"Anonymous pages"                =>        "818257",
+      :"Pages stored in compressor"     =>        "587616",
+      :"Pages occupied by compressor"   =>        "278734",
+      :Decompressions                 =>       "1107375",
+      :Compressions                   =>       "1904226",
+      :Pageins                        =>       "2532187",
+      :Pageouts                       =>          "1587",
+      :Swapins                        =>       "1062813",
+      :Swapouts                       =>       "1093920"
     }
     actual = Analyzer::execute(@read_data)
-    
+
     assert_equal(expected, actual)
   end
-  
+
   def test_analyze_empty
     test = lambda{|arg|
       expected = {}
       actual = Analyzer::execute(arg)
-      
+
       assert_equal(expected, actual)
     }
     test.call(nil)
